365246f02218e269ca83bf4ae366e694a90d3e07
Chufam <dchu1@csub.edu>
2026-03-06 09:29:04 -0800

    Added HUD, camera, and gameplay controller

--- gameplay.gd
+++ gameplay.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,8 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://gameplay.tscn")
+	var hud = $hudLayer/hud/position
+	hud.text = "Position: " + str($Player.position)
--- player.gd
+++ player.gd
@@ -1,8 +1,12 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed: float = 300.0
 
-func _process(delta: float) -> void:
+func get_input():
 	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = dir * speed
+
+func _physics_process(delta: float) -> void:
+	get_input()
 	look_at(get_global_mouse_position())
-	position += dir * speed * delta
+	move_and_collide(velocity * delta)
