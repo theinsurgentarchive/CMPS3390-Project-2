29502daef0f47a79de4ad59897728d5187d60bb0
Chufam <dchu1@csub.edu>
2026-03-06 09:45:33 -0800

    Fixed menu code after reorganization

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://gameplay.tscn")
+	get_tree().change_scene_to_file("res://Scenes/gameplay.tscn")
