8714da29a7de261bf184f3615dfc93478267b29f
Chufam <theinsurgentarchive@gmail.com>
2026-03-15 18:40:39 -0700

    fixed bug where other buttons in game over could be pressed and operated after a button was selected

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -3,6 +3,7 @@ extends Control
 var data: Database
 var score: Score
 var value: int
+var selected: bool = false
 
 func _ready() -> void:
 	# Ensure Database exists even if GameOver is run directly.
@@ -29,28 +30,37 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_button_pressed() -> void:
+	if selected:
+		return
 	var name: String = $UserInput.text.strip_edges()
 	if name == "":
 		return
 	_on_line_edit_text_submitted(name)
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
+	if selected:
+		return
 	var name: String = new_text.strip_edges()
 	if name == "":
 		return
-
 	data.addScore(value, name)
 	print_debug("GAME_OVER: submitted name='" + name + "' score=" + str(value))
 	$Score.text = "Score added to Leaderboard, returning to Main Menu."
+	selected = true
 	await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/mainMenu.tscn")
 
 
 func _on_exit_menu_pressed() -> void:
+	if selected:
+		return
+	selected = true
 	get_tree().change_scene_to_file("res://Scenes/mainMenu.tscn")
 
 
 func _on_exit_desktop_pressed() -> void:
+	if selected:
+		return
 	$Score.text = "Goodbye!!!"
 	await get_tree().create_timer(1.2).timeout
 	get_tree().quit()
